6d4daae1979c4491630694f01a3911c868b8791b
Anish Shukla <shuklaanish040@gmail.com>
2024-12-30 21:18:59 +0530

    kbc

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div className="app">
+      <div className="main">main</div>
+      <div className="pyramid">money</div>
+    </div>
+  );
+}
+
+export default App;
